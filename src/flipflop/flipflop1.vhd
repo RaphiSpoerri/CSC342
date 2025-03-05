39124540d93f14579ae4770d90f1ecc7d3f705d2
Raphi Spoerri <coolrustcean@gmail.com>
2025-03-05 23:10:33 +0000

    streamlined flipflop1

--- src/flipflop/flipflop1.vhd
+++ src/flipflop/flipflop1.vhd
@@ -9,16 +9,17 @@ use work.processor.nor_gate;
 entity flipflop1 is
 	port(
 		clk, d: in std_logic;
-		q, qnot: inout std_logic
+		q: inout std_logic
 	);
 end flipflop1;
 
 architecture behav of flipflop1 is
 	signal w1: std_logic := '0';
 	signal w2: std_logic := '1';
+	signal conn: std_logic;
 begin
-	nor1: nor_gate port map(w1, q, qnot);
-	nor2: nor_gate port map(w2, qnot, q);
+	nor1: nor_gate port map(w1, q, conn);
+	nor2: nor_gate port map(w2, conn, q);
 	process(clk) is begin
 		w1 <= clk and d;
 		w2 <= clk and not d;
--- test/flipflop.vhd
+++ test/flipflop.vhd
@@ -10,9 +10,9 @@ end testbench;
 
 architecture tab of testbench is
 	signal clk, d: std_logic;
-	signal q, qnot: std_logic := '0';
+	signal q: std_logic := '0';
 begin
-  ff: flipflop1 port map(clk => clk, d => d, q => q, qnot => qnot);
+  ff: flipflop1 port map(clk => clk, d => d, q => q);
 
   process is begin
 	d <= '0';
@@ -21,6 +21,7 @@ begin
 	if q /= '0' then
 		report "#1 Fail" severity error;
 	end if;
+	report std_logic'image(clk) & ":" & std_logic'image(d) & ":" & std_logic'image(q);
 	
 	d <= '1';
 	clk <= '0';
@@ -28,6 +29,7 @@ begin
 	if q /= '0' then
 		report "#2 Fail" severity error;
 	end if;
+	report std_logic'image(clk) & ":" & std_logic'image(d) & ":" & std_logic'image(q);
 
 	d <= '1';
 	clk <= '1';
@@ -35,6 +37,7 @@ begin
 	if q /= '1' then
 		report "#3 Fail" severity error;
 	end if;
+	report std_logic'image(clk) & ":" & std_logic'image(d) & ":" & std_logic'image(q);
 	
 
 	d <= '0';
@@ -43,6 +46,7 @@ begin
 	if q /= '1' then
 		report "#4 Fail" severity error;
 	end if;
+	report std_logic'image(clk) & ":" & std_logic'image(d) & ":" & std_logic'image(q);
 
 	d <= '0';
 	clk <= '1';
@@ -50,6 +54,7 @@ begin
 	if q /= '0' then
 		report "#5 Fail" severity error;
 	end if;
+	report std_logic'image(clk) & ":" & std_logic'image(d) & ":" & std_logic'image(q);
 
 
 	assert false report "Test done." severity note;
