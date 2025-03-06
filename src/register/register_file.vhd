4c0b2a49c7051c9d73513c1aa16fac247c010d4a
Raphael Spoerri <coolrustacean@gmail.com>
2025-03-05 22:04:19 -0500

    Wed Mar  5 22:04:19 EST 2025

--- src/register/register_file.vhd
+++ src/register/register_file.vhd
@@ -4,18 +4,33 @@
 library ieee, work;
 use ieee.std_logic_1164.all;
 use work.processor.register32;
+use work.processor.mux_4x1;
 
 entity register_file is port (
 	w: in std_logic;
-	d: in std_logic_vector(33 downto 0);
-	q1: out std_logic_vector(33 downto 0);
-	q2: out std_logic_vector(33 downto 0)
+	sel_d, sel_q1, sel_q0: in std_logic_vector(1 downto 0);
+	d: in std_logic_vector(31 downto 0);
+	q1: out std_logic_vector(31 downto 0);
+	q0: out std_logic_vector(31 downto 0)
 ); end entity;
 
-architecture behav of register_file is begin
---	r3: register32 port map(w, d, q1(31 downto 24));
---	r2: register32 port map(w, d, q1(23 downto 16));
---	r1: register32 port map(w, d, q1(15 downto 8));
---	r0: register32 port map(w, d, q1(7 downto 0));
+architecture behav of register_file is
+	signal bus0, bus1, bus2, bus3: std_logic_vector(31 downto 0);
+	signal w3, w2, w1, w0: std_logic; 
+begin
+	mux1: mux_4x1 port map(q1, bus0, bus1, bus2, bus3, sel_q1);
+	mux0: mux_4x1 port map(q0, bus0, bus1, bus2, bus3, sel_q0);
+
+	r3: register32 port map(clk => w3, d => d, q => bus3);
+	r2: register32 port map(clk => w2, d => d, q => bus2);
+	r1: register32 port map(clk => w1, d => d, q => bus1);
+	r0: register32 port map(clk => w0, d => d, q => bus0);
+
+	process(w, sel_d) is begin
+		w3 <= w and sel_d(1) and sel_d(0);
+		w2 <= w and sel_d(1) and not sel_d(0);
+		w1 <= w and not sel_d(1) and sel_d(0); 
+		w0 <= w and not sel_d(1) and not sel_d(0);
+	end process;
 end architecture;
 
