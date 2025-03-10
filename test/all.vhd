e64ab286b4112529dd987c92ee063b23106e3b47
Raphi Spoerri <coolrustcean@gmail.com>
2025-03-10 01:54:38 +0000

    Mon Mar 10 01:54:38 UTC 2025

--- test/all.vhd
+++ test/all.vhd
@@ -15,8 +15,8 @@ architecture tb of testbench is
 	signal regs: std_logic_vector(35 downto 0);
 	type Instructions is array(natural range <>) of std_logic_vector(31 downto 0);
 	constant program: Instructions := (
-		addi & r1 & r1 & "00000000" & "00000010",
-		addi & r0 & r0 & "00000000" & "00000010",
+		addi & r1 & r0 & "00000000" & "00000010",
+		addi & r0 & r2 & "00000000" & "00000010",
 		add & r2 & r1 & r0 & unused
 	);
 begin
@@ -31,6 +31,27 @@ begin
 		clk <= '1';
 		wait for 15 ns;
 		clk <= '0';
+		report to_string(res);
+		dest <= "11";
+		reg2 <= zeros32(23 downto 0) & "00001111";
+		wait for 5 ns;
+		clk <= '1';
+		wait for 15 ns;
+		clk <= '0';
+		report to_string(res);
+		reg1 <= "11";
+		wait for 5 ns;
+		clk <= '1';
+		wait for 15 ns;
+		clk <= '0';
+		report to_string(res);
+
+
+		wait for 5 ns;
+		clk <= '1';
+		wait for 15 ns;
+		clk <= '0';
+		report to_string(res);
 		wait for 5 ns;
 
 		for i in 0 to program'length - 1 loop
@@ -38,14 +59,25 @@ begin
 			stmt <= program(i);
 			exec <= '1';
 			wait for 15 ns;
-
-			report to_string(flags);
 			exec <= '0';
+			report to_string(flags) & ":" & std_logic'image(flags(0));
+			sub <= flags(0);
+			signed <= flags(1);
+			imm <= flags(2);
+			dest <= regs(1 downto 0);
+			reg1 <= regs(3 downto 2);
+			reg2 <= regs(35 downto 4);
+			report "regs " & to_string(dest) & " " & to_string(reg1) & " " & to_string(reg2);
+			wait for 1 ns;
+			clk <= '1';
+			wait for 15 ns;
+			clk <= '0';
 			wait for 5 ns;
+			report to_string(res);
 		end loop;
 
 		--report to_string(stmt);
-		report to_string(res);
+		--report to_string(res);
 		wait;
 	end process;
 end architecture;
