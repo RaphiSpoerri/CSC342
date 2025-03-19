86ec66e077e32d4cf3cda50bd17c30d16982e103
Raphael Spoerri <coolrustacean@gmail.com>
2025-03-19 12:37:06 -0400

    Wed Mar 19 12:37:06 EDT 2025

---
+++ project2/floats.asm
@@ -0,0 +1,18 @@
+
+
+
+.data
+float_anon1: .float 1.1
+float_anon2: .float 2.2
+
+.text
+main:
+	lwc1 $f1, float_anon1
+	lwc1 $f2, float_anon2
+	add.s $f12, $f1, $f2
+	
+	li $v0, 2 # print float
+	syscall
+	
+	li $v0, 10 # exit
+	syscall
